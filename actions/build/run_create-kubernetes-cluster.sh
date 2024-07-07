d156b591c3be9008c8c07cb9d7bfb2f724fc7a81
Esteban Cruz <carlos.cruz19@outlook.fr>
2024-07-07 11:52:00 -0600

    Add instructions to main script to create and join worker nodes

--- actions/build/run_create-kubernetes-cluster.sh
+++ actions/build/run_create-kubernetes-cluster.sh
@@ -16,8 +16,8 @@ set -eo pipefail
 
 export LOG_DIR="./logs/"
 export LOG_FILE="${LOG_DIR}/kubernetes-in-incus-$(date +"%Y%m%d").log"
-export WRITE_TO_LOGFILE=true
-export DEBUG=true
+export WRITE_TO_LOGFILE=false
+export DEBUG=false
 
 #########################################################
 # Bash functions definition #
@@ -46,7 +46,7 @@ CONTROL_PLANE_KUBEADM_VERSION="1.30.0-1.1"
 CONTROL_PLANE_KUBELET_VERSION="1.30.0-1.1"
 CONTROL_PLANE_KUBECTL_VERSION="1.30.0-1.1"
 POD_NETWORK_ADDON="https://reweave.azurewebsites.net/k8s/v1.30/net.yaml"
-NO_WORKER_NODES=2
+NO_WORKER_NODES=1
 
 #########################################################
 # Main Script #
@@ -111,7 +111,7 @@ if ! ./actions/incus/create-profile.sh \
   --static-address="${control_plane_cidr_adress}" \
   --gateway="${default_gateway_address}"; 
 then
-  log_error "Failed to run script to create incus profile"
+  log_error "Failed to run script to create incus profile for control plane"
   exit 1
 fi
 
@@ -123,7 +123,7 @@ if ! ./actions/incus/launch-instance.sh \
   --cpus="${CONTROL_PLANE_CPUS}" \
   --memory="${CONTROL_PLANE_MEMORY}";
 then
-  log_error "Failed to run script to launch incus instance"
+  log_error "Failed to run script to launch incus instance for control-plane"
   exit 1
 fi
 
@@ -178,11 +178,90 @@ log_info "Applying customizations"
 # -------------------------------------------------------
 # Worker Nodes Section #
 # -------------------------------------------------------
+for (( i = 1; i <= $NO_WORKER_NODES; i++ ))
+do
+    worker_hostname="worker-$i"
 
-# TODO(Esteban Cruz):... 
+    log_info "Getting ip address for worker '$worker_hostname'"
+    if ! command_output=$(get_next_ipv4 "$control_plane_address" $(($i+1))); then
+        log_error "$command_output"
+        exit 1
+    fi
+
+    worker_address="$command_output"
+    worker_cidr_address="${worker_address}/${network_submask}"
+    log_info "IP address $worker_cidr_address will be assigned to the worker node '${worker_hostname}'"
+
+
+    # Create incus profile for worker node
+    if ! ./actions/incus/create-profile.sh \
+      --profile-name="${worker_hostname}" \
+      --network-name="${NETWORK_NAME}" \
+      --static-address="${worker_cidr_address}" \
+      --gateway="${default_gateway_address}"; 
+    then
+      log_error "Failed to run script to create incus profile for worker 'worker_hostname'"
+      exit 1
+    fi
+
+    # Launching Incus worker node instance
+    if ! ./actions/incus/launch-instance.sh \
+      --image="${CONTROL_PLANE_BASE_IMAGE}" \
+      --hostname="${worker_hostname}" \
+      --profile="${worker_hostname}" \
+      --cpus="${CONTROL_PLANE_CPUS}" \
+      --memory="${CONTROL_PLANE_MEMORY}";
+    then
+      log_error "Failed to run script to launch incus instance for worker node"
+      exit 1
+    fi
+
+    # Waiting for worker instance to be ready to exec scripts
+    ./actions/incus/wait-for-instance.sh \
+      --instance-name="${worker_hostname}" \
+      --max-retries=10
+
+    # ------------------------------------
+    # Worker Provisioning Section #
+    # ------------------------------------
+    # Install and configure Containerd
+    ./actions/incus/exec-script.sh \
+      --incus-cwd "/tmp/kubeadm/provisioning" \
+      --instance-name "${worker_hostname}" \
+      --script "./actions/build/provision-scripts/containerd.sh" \
+      --containerd-version "${CONTROL_PLANE_CONTAINERD_VERSION}"
+
+    # Apply Network settings
+    ./actions/incus/exec-script.sh \
+      --incus-cwd "/tmp/kubeadm/provisioning" \
+      --instance-name "${worker_hostname}" \
+      --script "./actions/build/provision-scripts/network-configurations.sh"
+
+    # Installing kubernetes components
+    ./actions/incus/exec-script.sh \
+      --incus-cwd "/tmp/kubeadm/provisioning" \
+      --instance-name "${worker_hostname}" \
+      --script "./actions/build/provision-scripts/kubernetes-components.sh" \
+      --repository-version "${CONTROL_PLANE_KUBERNETES_REPOSITORY_VERSION}" \
+      --kubeadm "${CONTROL_PLANE_KUBEADM_VERSION}" \
+      --kubelet "${CONTROL_PLANE_KUBELET_VERSION}"
+
+    
+    if ! command_output=$(incus exec ${CONTROL_PLANE_HOSTNAME} -- /bin/bash -c "kubeadm token create --print-join-command" 2>&1); then
+      log_error "Failed to create kubeadm token for worker $worker_hostname"
+      exit 1
+    else
+      join_command=$command_output
+      if ! command_output=$(incus exec ${worker_hostname} -- /bin/bash -c "${join_command}"); then
+        log_error "Worker node $worker_hostname failed to join the cluster" "$command_output"
+        exit 1
+      fi
+    fi
+    log_info "Worker node ${worker_hostname} joined the cluster successfully"
+done
 
 #########################################################
 # Finalization #
 #########################################################
 
-
+log_info "Successfully created the kubernetes cluster"
